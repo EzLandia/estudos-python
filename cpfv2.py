38935c6a57b5d17c5ae291f0ef68a6be6e002498
EzLandia <82134080+EzLandia@users.noreply.github.com>
2021-05-30 12:41:01 -0300

    Update cpfv2.py
    Váriaveis renomeadas e com menos linhas.

--- cpfv2.py
+++ cpfv2.py
@@ -1,29 +1,26 @@
 def multiplicar(list, i, f, p):
-    dg = 0
-    s = 0
-    for d in range(i, f, p):
-        s += list[dg]*d
-        dg += 1
-    return s
+    inicio = 0
+    soma = 0
+    for digito in range(i, f, p):
+        soma += list[inicio]*digito # Multiplica e adiciona a soma a váriavel para verificação dos digitos.
+        inicio += 1
+    return soma
 
 
 while True:
     cpf = []
-    num = list(input('Digite seu CPF: '))
-    for d in num:
-        if d.isnumeric() == True:
-            cpf.append(int(d))
+    numero = list(input('Digite seu CPF: ')) # Pede ao usuario para informar o CPF a ser verificado.
+    for digito in numero:
+        if digito.isnumeric() == True: # Verifica na váriavel numero se os elementos são númericos.
+            cpf.append(int(digito)) # Caso sejam númericos, são adicionados a lista cpf.
     print(f'\nCPF informado: {cpf[0:11]}',end='')
-    if len(cpf) == 11:
+    if len(cpf) == 11: # Verifica se o tamanho da lista CPF contem 11 digitos, se sim, segue para verificação.
         print('\nVerificando se esse CPF é válido...\n')
         break
-    else:
+    else: # Caso o tamanho da lista tenha mais ou menos números, repete o loop e pede para inserir novamente.
         print('\nParece que você não digitou um CPF, tente novamente.\n')
-v1 = 11-(multiplicar(cpf, 10, 1, -1)%11)
-if v1 >= 10: v1 = 0
-v2 = 11-((multiplicar(cpf, 11, 2, -1)+(v1*2))%11)
-if v2 >= 10: v2 = 0
-if v1 == cpf[9] and v2 == cpf[10]:
-    print('CPF VÁLIDO!')
-else:
-    print('CPF INVALIDO!')
+verificador1 = 11-(multiplicar(cpf, 10, 1, -1)%11) # Envia os dados para a função multiplicar para obter o 1º digito verificador.
+if verificador1 >= 10: verificador1 = 0 # Caso o primeiro digito seja igual ou maior que 10, ele é considerado zero.
+verificador2 = 11-((multiplicar(cpf, 11, 2, -1)+(verificador1*2))%11) # Envia os dados para a função multiplicar e verifica o 2º digito.
+if verificador2 >= 10: verificador2 = 0 # Caso o segundo digito seja igual ou maior a 10, tbm será considerado zero.
+print('CPF VÁLIDO' if verificador1 == cpf[9] and verificador2 == cpf[10] else 'CPF INVALIDO') #Compara os digitos e mostra o resultado.
